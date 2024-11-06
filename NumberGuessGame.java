b4ab54c045ab858f644e116464002b8b6d1ef5a1
JamesBunny404 <jamesbunny1212@gmail.com>
2024-11-06 10:34:27 -0800

    Update NumberGuessGame.java

--- NumberGuessGame.java
+++ NumberGuessGame.java
@@ -6,11 +6,11 @@ public class NumberGuessGame {
         Random random = new Random();
         int targetNumber = random.nextInt(150) + 1; 
         int limit = 7;
-        int attemLeft = limit; // this can be optmized i think but idk how
+        int attemptLeft = limit; // this can be optmized i think but idk how
 
         Scanner scanner = new Scanner(System.in);
 
-        while (attemptsLeft > 0) {
+        while (attemptLeft > 0) {
             System.out.print("Guess the number (1-150): ");
             int userGuess = scanner.nextInt();
             
@@ -18,17 +18,17 @@ public class NumberGuessGame {
                 System.out.println("no"); // prevent people from guessing more then the largest num
                 
             } else if (userGuess > targetNumber) {
-                attemLeft--;
-                System.out.println("Less than " + userGuess + ". Attempts left: " + attemLeft);
+                attemptLeft--;
+                System.out.println("Less than " + userGuess + ". Attempts left: " + attemptLeft);
             } else if (userGuess < targetNumber) {
-                attemLeft--;
-                System.out.println("Greater than " + userGuess + ". Attempts left: " + attemLeft);
+                attemptLeft--;
+                System.out.println("Greater than " + userGuess + ". Attempts left: " + attemptLeft);
             } else {
                 System.out.println("correct");
                 break;
             }
 
-            if (attemLeft == 0) {
+            if (attemptLeft == 0) {
                 System.out.println("The correct number was: " + targetNumber);
             }
         }
